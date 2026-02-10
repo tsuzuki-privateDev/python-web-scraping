0008a4a1de7aacb59f3ffb6c263afffd4fe63ac8
tadatsugu <tsuzuki2215@gmail.com>
2026-02-10 20:55:42 +0900

    実サイトスクレイピングをCLIで実行可能に

--- src/s6_python_blog.py
+++ src/s6_python_blog.py
@@ -2,10 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 from pathlib import Path
+import argparse
 
 
 def main():
-    url = "https://blog.python.org/"
+    # パーサーの作成
+    parser = argparse.ArgumentParser()
+    # コマンドライン引数の設定
+    parser.add_argument("--url", required=True)
+    parser.add_argument("--out", default="outputs/python_blog.csv")
+    # 引数を解析
+    args = parser.parse_args()
+    url = args.url
     response = requests.get(url, timeout=10)  # 10秒待つ
 
     if response.status_code == 200:
@@ -17,13 +25,13 @@ def main():
 
         for item in items:
             title = item.a.text
-            url = item.a["href"]
-            item_dict = {"title": title, "url": url}
+            post_url = item.a["href"]
+            item_dict = {"title": title, "url": post_url}
             results.append(item_dict)
 
         Path("outputs").mkdir(exist_ok=True)
 
-        with open("outputs/python_blog.csv", "w", newline="", encoding="utf-8") as f:
+        with open(args.out, "w", newline="", encoding="utf-8") as f:
             writer = csv.DictWriter(f, fieldnames=["title", "url"])
             writer.writeheader()
             for row in results:
